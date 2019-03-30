bb1c9c5a4ef31cdf6030bef73f373a10d4e95f9c
samueltrejo <47835231+samueltrejo@users.noreply.github.com>
2019-03-30 18:51:07 -0500

    syntax error fixes

--- main.js
+++ main.js
@@ -1,33 +1,25 @@
 const arrayOfFounders = [
-    {
-        name: 'Heath',
-        description: "Heath makes the best playlists with drake for the crew to listen to in warehouse! His hip-HOPPS playlist is part of out trade secrets",
-        image:'https://i.pinimg.com/originals/11/af/65/11af65a0bc6e6949a005a8cf12db4ee5.jpg
- https://i.pinimg.com/originals/11/af/65/11af65a0bc6e6949a005a8cf12db4ee5.jpg
- ',
-    },
-    {
-        name: 'Samuel',
-        description: "Is the team lead of this MALT-ly crew!",
-        image: 'https://i.pinimg.com/originals/79/7d/f0/797df028d0dd812449584089005ce670.jpg
- https://i.pinimg.com/originals/79/7d/f0/797df028d0dd812449584089005ce670.jpg
- '
-    },
-    {
-        name: 'Jen',
-        description: "Jen picks the finest barley! Every one else can BARELY compete",
-        image: 'https://hips.hearstapps.com/hbz.h-cdn.co/assets/16/34/hbz-strawberry-blonde-blake-lively.jpg?crop=1.0xw:1xh;center,top&resize=480:*
- https://hips.hearstapps.com/hbz.h-cdn.co/assets/16/34/hbz-strawberry-blonde-blake-lively.jpg?crop=1.0xw:1xh;center,top&resize=480:*
- '
-    },
-    {
-        name: 'Matthew',
-        description: "Is mostly a janitor but some time comes up with crazy beer ideas time to time!",
-        image: 'https://m.media-amazon.com/images/M/MV5BODZjM2UzOTEtMTk5MC00NjZlLWJiM2EtMThhMDJhMGZkOGFkXkEyXkFqcGdeQXVyODQwMzE5OQ@@._V1_UX140_CR0,0,140,209_AL_.jpg
- https://m.media-amazon.com/images/M/MV5BODZjM2UzOTEtMTk5MC00NjZlLWJiM2EtMThhMDJhMGZkOGFkXkEyXkFqcGdeQXVyODQwMzE5OQ@@._V1_UX140_CR0,0,140,209_AL_.jpg
- '
-    },
- ];
+  {
+    name: 'Heath',
+    description: "Heath makes the best playlists with drake for the crew to listen to in warehouse! His hip-HOPPS playlist is part of out trade secrets",
+    image:'https://i.pinimg.com/originals/11/af/65/11af65a0bc6e6949a005a8cf12db4ee5.jpg',
+  },
+  {
+    name: 'Samuel',
+    description: "Is the team lead of this MALT-ly crew!",
+    image: 'https://i.pinimg.com/originals/79/7d/f0/797df028d0dd812449584089005ce670.jpg',
+  },
+  {
+    name: 'Jen',
+    description: "Jen picks the finest barley! Every one else can BARELY compete",
+    image: 'https://hips.hearstapps.com/hbz.h-cdn.co/assets/16/34/hbz-strawberry-blonde-blake-lively.jpg?crop=1.0xw:1xh;center,top&resize=480:*',
+  },
+{
+    name: 'Matthew',
+    description: "Is mostly a janitor but some time comes up with crazy beer ideas time to time!",
+    image: 'https://m.media-amazon.com/images/M/MV5BODZjM2UzOTEtMTk5MC00NjZlLWJiM2EtMThhMDJhMGZkOGFkXkEyXkFqcGdeQXVyODQwMzE5OQ@@._V1_UX140_CR0,0,140,209_AL_.jpg'
+  },
+];
  
  const printToDom = (divId, textToPrint) => {
     const selectedDiv = document.getElementById(divId);
@@ -56,4 +48,3 @@ const arrayOfFounders = [
     };
  
     init();
- //const arrayOfFounders[];
