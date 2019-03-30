b921af8b5c04d982a45af204b68a1e0672678da8
Jennifer Lipps <jjlipps@gmail.com>
2019-03-30 13:57:18 -0500

    started beer array

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-console.log("yo yo yo");
+const arrayOfBeers = [
+    {
+     name: "Citra OG",
+     type: "American wheat",
+     style: "Wheat",
+     imageUrl: "blank",
+     ingredients: "malted wheat, Noble hops, yeast, water",
+     ibu: 25,
+     abv: 4.5
+    },
+    {
+     name: "Heart Healthy Oats",
+     type: "Oatmeal Stout",
+     style: "Ales",
+     imageUrl: "blank",
+     ingredients: "malted barley, hops, yeast, water",
+     ibu: 33,
+     abv: 3.5
+    },
+    {
+     name: "Heart Healthy Oats",
+     type: "Oatmeal Stout",
+     style: "Ales",
+     imageUrl: "blank",
+     ingredients: "malted barley, hops, yeast, water",
+     ibu: 33,
+     abv: 3.5
+    },
+    
+];
+
+console.log(arrayOfBeers);
